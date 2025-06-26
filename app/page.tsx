8cab2611bb833d4bbaf82d2bb6443478d2ba65ad
cryptoXseeder <cryptoxseeder@gmail.com>
2025-06-26 15:42:26 +0530

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Welcome to Tokenomics Zone</h1>
-      <p>👉 Go to <a href="/tokenomics">Tokenomics Tool</a></p>
-    </div>
-  );
-}
