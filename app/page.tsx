c84f3ba99d5ac894ade8ad13efcbed10c7981b46
cryptoXseeder <cryptoxseeder@gmail.com>
2025-06-26 14:59:26 +0530

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Tokenomics Zone</h1>
+      <p>To get started, go to <a href="/tokenomics">/tokenomics</a></p>
+    </div>
+  );
+}
